6c6405d0baa4f285df328d486bc01131b7a89d03
Kelvin Ferreira <k30v1n@users.noreply.github.com>
2018-01-19 16:47:38 -0200

    QuickSort. Ainda não está ordenando o array totalmente.

---
+++ Algoritmos/Algoritmos.Core.Tests/Order/QuickSortTests.cs
@@ -0,0 +1,20 @@
+﻿using Algoritmos.Core.Order;
+using Algoritmos.Core.Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algoritmos.Core.Tests.Order
+{
+    public class QuickSortTests
+    {
+        [Fact]
+        public void NormalOrder()
+        {
+            int[] resultado = QuickSortOrder.Order(OrderData.NormalArrayUnsorted());
+
+            Assert.Equal(resultado, OrderData.NormalArraySorted());
+        }
+    }
+}
---
+++ Algoritmos/Algoritmos.Core/Order/QuickSortOrder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmos.Core.Order
+{
+    public class QuickSortOrder
+    {
+        public static int[] Order(int[] arr)
+        {
+            return quickSort(arr, 0, arr.Length - 1);
+        }
+
+        private static int[] quickSort(int[] arr, int initVet, int endVet)
+        {
+            // TODO: Corrigir
+            int i = 0, j = 0, pivo = 0;
+
+            i = initVet;
+            j = endVet;
+            pivo = arr[(initVet + endVet) / 2];
+
+            while (i < j)
+            {
+                while (i < endVet && arr[i] <= pivo)
+                    i++;
+                while (j > initVet && arr[j] > pivo)
+                    j--;
+
+                if (i < j)
+                    (arr[i], arr[j]) = (arr[j], arr[i]);
+
+                i++;
+                j--;
+            }
+            System.Diagnostics.Debugger.Log(1, "", String.Join(", ", arr)+"\n");
+            if (j > initVet)
+                quickSort(arr, initVet, j);
+            if (i < endVet)
+                quickSort(arr, j + 1, endVet);
+
+            return arr;
+        }
+    }
+}
